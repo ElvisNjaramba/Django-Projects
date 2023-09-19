5990f9113d8fefed75415c04e41d8483b8b74ced
Tariq Marcel <kigutanjaramba@gmail.com>
2023-09-19 12:21:42 +0300

    first commit

---
+++ bookscraper/bookscraper/pipelines.py
@@ -0,0 +1,157 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import mysql.connector
+from itemadapter import ItemAdapter
+
+
+class BookscraperPipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        # strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+
+        # category & product type --> switch to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        # price --> convert to float productexcl
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')  # Remove the pound sign
+            adapter[price_key] = float(value)
+
+        # Availability --> extract number of books in stock
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+
+        # Reviews --> convert string to number
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+
+        # Stars --> convert text to number
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == "zero":
+            adapter['stars'] = 0
+
+        elif stars_text_value == "one":
+            adapter['stars'] = 1
+
+        elif stars_text_value == "two":
+            adapter['stars'] = 2
+
+        elif stars_text_value == "three":
+            adapter['stars'] = 3
+
+        elif stars_text_value == "four":
+            adapter['stars'] = 4
+
+        elif stars_text_value == "five":
+            adapter['stars'] = 5
+
+        return item
+
+
+class SaveToMySqlPipeline:
+
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='books'
+        )
+
+        self.cur = self.conn.cursor()
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS `books` (
+            id int NOT NULL auto_increment,
+            url VARCHAR(255),
+            title text,
+            upc VARCHAR(255),
+            product_type VARCHAR(255),
+            price_excl_tax DECIMAL,
+            price_incl_tax DECIMAL,
+            tax DECIMAL,
+            price DECIMAL,
+            availability INTEGER,
+            num_reviews INTEGER,
+            starS INTEGER,
+            category VARCHAR(255),
+            description text,
+            PRIMARY KEY (id)
+        )
+        """)
+
+    def process_item(self, item, spider):
+        self.cur.execute("""
+        INSERT INTO books (
+            url,
+            title,
+            upc,
+            product_type,
+            price_excl_tax,
+            price_incl_tax,
+            tax,
+            price,
+            availability,
+            num_reviews,
+            stars,
+            category,
+            description
+        ) VALUES (
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s
+        )
+        """, (
+            item['url'],
+            item['title'],
+            item['upc'],
+            item['product_type'],
+            item['price_excl_tax'],  # Make sure this field exists in the item
+            item['price_incl_tax'],
+            item['tax'],
+            item['price'],
+            item['availability'],
+            item['num_reviews'],
+            item['stars'],
+            item['category'],
+            # Make sure this field exists in the item
+            str(item['description'][0])
+        ))
+
+        self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
